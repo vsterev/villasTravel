6b4852db39a0f3eb420aa505ea9a86e407133992
vasko <vsterev@gmail.com>
2020-08-12 08:51:13 +0300

    bokking page bootsraped

--- villas/src/components/bookVilla/index.jsx
+++ villas/src/components/bookVilla/index.jsx
@@ -3,6 +3,7 @@ import { useParams, useHistory } from 'react-router-dom'
 import villaService from '../../services/villaService'
 import reservationService from '../../services/reservationService'
 import parseCookie from '../../utils/parseCookie'
+import { Cards, Col, Form, Container, Row, Button, InputGroup, FormControl } from 'react-bootstrap'
 import './style.css'
 const BookVilla = () => {
     const [villaName, setVillaName] = useState('')
@@ -37,60 +38,126 @@ const BookVilla = () => {
                 setImageUrl(imageUrl)
             })
             .catch(err => console.log(err))
-        console.log(isAgree)
-        // return function () {
-        //     setLikes([])
-        // }
     }, [])
     const renderNames = () => {
         let i
         let t = [];
         for (i = 0; i < beds; i++) {
-            t.push(<div key={i}><label> Client {i + 1}:<input type="text" name={`client${i + 1}`} placeholder="name surname" /></label></div>)
+
+            t.push(
+                // <div key={i}><label>tourist {i + 1}: <input type="text" name={`client${i + 1}`} placeholder="name surname" /></label></div>
+
+
+                <InputGroup key={i} className="mb-3" >
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>First and last name</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl name={`client${i + 1}first`} />
+                    <FormControl name={`client${i + 1}second`} />
+                </InputGroup >
+
+            )
+
         }
-        t.push(<div key={beds}><label>Comment: <textarea cols="65" rows="5" name="comment" placeholder="type your comments here" /></label></div>)
+        t.push(
+            // <div key={beds}><label>comment: <textarea cols="65" rows="5" name="comment" placeholder="type your comments here" /></label></div>
+            <InputGroup key={i}>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Comment</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl as="textarea" aria-label="With textarea" name="comment" placeholder="type your comments here - optional"/>
+          </InputGroup>
+            )
         return t;
     }
+    // const renderNames = () => {
+    //     let i
+    //     let t = [];
+    //     for (i = 0; i < beds; i++) {
+    //         t.push(<div key={i}><label>tourist {i + 1}: <input type="text" name={`client${i + 1}`} placeholder="name surname" /></label></div>)
+    //     }
+    //     t.push(<div key={beds}><label>comment: <textarea cols="65" rows="5" name="comment" placeholder="type your comments here" /></label></div>)
+    //     return t;
+    // }
     const reservationHandler = (e) => {
         e.preventDefault()
         const clients = [];
         let i;
         let currValue = ''
         for (i = 0; i < beds; i++) {
-            currValue = e.target[`client${i + 1}`].value
+            const currFirst = e.target[`client${i + 1}first`].value
+            const currSecond = e.target[`client${i + 1}second`].value
+            currValue = `${currFirst} ${currSecond}`
             clients.push(currValue)
         }
-        console.log(clients)
         const comment = e.target.comment.value
         const dataParams = { clients, villaId, comment }
         console.log(dataParams)
-        reservationService.createReservation(dataParams, token)
-            .then(data => {
-                if (!data.status) {
-                    setMsg(data.msg)
-                    return
-                }
-                history.push('/profile')
-            })
-            .catch(err => console.log(err))
+        // reservationService.createReservation(dataParams, token) //Fa go pusna spira podavaneto na zaivkata kym bazata
+        //     .then(data => {
+        //         if (!data.status) {
+        //             setMsg(data.msg)
+        //             return
+        //         }
+        //         history.push('/profile')
+        //     })
+        //     .catch(err => console.log(err))
     }
     return (
-        <div>
-                <h2 className="header">Book - {villaName} in {region}</h2>
-                <div><img src={imageUrl} width="950" alt={villaName} /></div>
-                <div>date chek-in: {date} for {nights} nights</div>
-                <div>maximum acommodated person: {beds}</div>
-                <div>price: {price} EUR</div>
-                <div><textarea name="priceDescription" value={priceDescription} disabled cols="60" rows='6'></textarea></div>
-                <div><label>  confirm policy and price<input type="checkbox" checked={isAgree} onChange={() => setIsAgree(!isAgree)} id="checkbox" name="checkbox" /></label></div>
-                {isAgree &&
-                    <form onSubmit={reservationHandler}>
-                        {renderNames()}
-                        <div>
-                            <button disabled={!isAgree} type="submit" >Make reservation</button>
-                        </div>
-                    </form>}
-        </div>
+        <Container>
+            <br />
+            <h2 className="header">Book - {villaName} in {region}</h2>
+            <Row>
+                <Col>
+                </Col>
+                <Col className="justify-content-md-center">
+                    <br />
+                    <div><img src={imageUrl} width="650" alt={villaName} /></div>
+                </Col>
+                <Col>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                </Col>
+                <Col className="justify-content-md-center">
+                    <br />
+                    <div>date chek-in: {date} for {nights} nights</div>
+                    <div>maximum person: {beds}</div>
+                    <div>price: <b>{price}</b> EUR</div>
+                </Col>
+                <Col>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                </Col>
+                <Col>
+                    <br />
+                    <Form>
+                        <Form.Group>
+                            <Form.Label><i>price terms & conditions:</i></Form.Label>
+                            <Form.Control as="textarea" name='priceDescription' value={priceDescription} disabled rows={6} />
+                        </Form.Group>
+                    </Form>
+                </Col>
+                <Col>
+                </Col>
+            </Row>
+
+
+            <div className="names">
+                <div><label><input type="checkbox" checked={isAgree} onChange={() => setIsAgree(!isAgree)} id="checkbox" name="checkbox" />{' '}confirm price policy</label></div>
+
+                <form onSubmit={reservationHandler}>
+                    {isAgree && renderNames()}
+                    <div>
+                        <br />
+                        <Button disabled={!isAgree} type="submit" >Book it</Button>
+                    </div>
+                </form>
+            </div>
+        </Container >
     )
 }
 export default BookVilla
