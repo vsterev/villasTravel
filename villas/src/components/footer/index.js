c04bef4ddd25719045d6db4584a0b476317bc97f
vasko <vsterev@gmail.com>
2020-08-13 11:13:09 +0300

    banners fixed

--- villas/src/components/footer/index.js
+++ villas/src/components/footer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const FooterPage = () => {
   return (
-    <footer id="sticky-footer" class="py-3 bg-light">
-      <div class="container">
-        <p class="m-0 text-center ">Copyright © Villas Choise</p>
+    <footer id="sticky-footer" className="py-3 bg-light">
+      <div className="container">
+        <p className="m-0 text-center ">Copyright © Villas Choise</p>
       </div>
     </footer>
   )
