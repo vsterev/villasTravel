fa06329d5c51bc09553aad2189059b3b7c0617f6
vasko <vsterev@gmail.com>
2020-08-26 00:19:33 +0300

    config change

--- villas-API/config/config.js
+++ villas-API/config/config.js
@@ -2,14 +2,13 @@ const env = process.env.NODE_ENV || 'cloud';
 const DB_PASS = process.env.DB_PASS;
 const DB_USER = process.env.DB_USER;
 const config = {
-    cloud: {
-        port: process.env.PORT || 4000, //3000
-        // dataBaseUrl: `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-azuwr.mongodb.net/villas`
-        dataBaseUrl: `mongodb+srv://vasko:vas2014eli@cluster0-azuwr.mongodb.net/villas`
-    },
-    local: {
-        port: 4000,
-        dataBaseUrl: 'mongodb://127.0.0.1:27017/villas'
-    }
+  cloud: {
+    port: process.env.PORT || 4000, //3000
+    dataBaseUrl: `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-azuwr.mongodb.net/villas`,
+  },
+  local: {
+    port: 4000,
+    dataBaseUrl: 'mongodb://127.0.0.1:27017/villas',
+  },
 };
 module.exports = config[env];
